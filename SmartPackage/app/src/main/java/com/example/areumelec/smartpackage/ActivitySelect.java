a0f9a706ce1fe19097029d9b5f1275ae06a40633
hb1love <heebuma@gmail.com>
2016-10-13 22:10:32 +0900

    Client PushNotification

--- SmartPackage/app/src/main/java/com/example/areumelec/smartpackage/ActivitySelect.java
+++ SmartPackage/app/src/main/java/com/example/areumelec/smartpackage/ActivitySelect.java
@@ -6,7 +6,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.LinearLayout;
 
-import com.example.areumelec.Model.BackPressCloseHandler;
 import com.google.firebase.messaging.FirebaseMessaging;
 
 public class ActivitySelect extends AppCompatActivity {
@@ -60,4 +59,41 @@ public class ActivitySelect extends AppCompatActivity {
                 startActivity(intent2);
         }
     }
+
+//    if (getIntent().getExtras() != null) {
+//        for (String key : getIntent().getExtras().keySet()) {
+//            Object value = getIntent().getExtras().get(key);
+//            Log.d(TAG, "Key: " + key + " Value: " + value);
+//        }
+//    }
+//    // [END handle_data_extras]
+//
+//    Button subscribeButton = (Button) findViewById(R.id.subscribeButton);
+//    subscribeButton.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            // [START subscribe_topics]
+//            FirebaseMessaging.getInstance().subscribeToTopic("news");
+//            // [END subscribe_topics]
+//
+//            // Log and toast
+//            String msg = getString(R.string.msg_subscribed);
+//            Log.d(TAG, msg);
+//            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        }
+//    });
+//
+//    Button logTokenButton = (Button) findViewById(R.id.logTokenButton);
+//    logTokenButton.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            // Get token
+//            String token = FirebaseInstanceId.getInstance().getToken();
+//
+//            // Log and toast
+//            String msg = getString(R.string.msg_token_fmt, token);
+//            Log.d(TAG, msg);
+//            Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+//        }
+//    });
 }
---
+++ SmartPackage/app/src/main/java/com/example/areumelec/smartpackage/MyFirebaseInstanceIDService.java
@@ -0,0 +1,33 @@
+package com.example.areumelec.smartpackage;
+
+/**
+ * Created by hbkim on 2016. 9. 24..
+ */
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    // [START refresh_token]
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        sendRegistrationToServer(refreshedToken);
+    }
+
+    private void sendRegistrationToServer(String token) {
+        // TODO: Implement this method to send token to your app server.
+    }
+
+
+}
