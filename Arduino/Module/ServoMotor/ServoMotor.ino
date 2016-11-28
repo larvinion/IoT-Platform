f4ddcc778625901cec55652d69047904da84cd53
Larva <ssh9112@naver.com>
2016-11-28 21:48:29 +0900

    Modify Servo_blue

--- Arduino/Module/ServoMotor/ServoMotor.ino
+++ Arduino/Module/ServoMotor/ServoMotor.ino
@@ -1,20 +1,50 @@
 #include <Servo.h>
+uint8_t buffer[256];
+uint8_t index = 0;
+uint8_t data;
+int degree = 18;
 
+// 5V    
 Servo myservo;
 int pos = 0;
 
 void setup() {
   myservo.attach(9);
+  Serial.begin(9600);
+  Serial1.begin(9600);
 }
 
 void loop() {
-  for(pos = 0; pos <=180; pos++){
-    myservo.write(pos);
-    delay(15);
+  while(Serial1.available()){
+    data = Serial1.read();
+    buffer[index++] = data;
+    if(index == 256 || data =='\0')break;
+    
+    delay(1);
   }
   
-  for(pos = 180; pos >=0; pos--){
-    myservo.write(pos);
-    delay(15);
+  if(buffer[0] == 'r'){
+    int now = degree * 5;
+    int next;
+    degree = degree + 1;
+    next = degree * 5;
+    
+    for(pos = now; pos <=next; pos++){
+      myservo.write(pos);
+      delay(50);
+    }
   }
+  else if(buffer[0] == 'l'){
+    int now = degree * 5;
+    int next;
+    degree = degree - 1;
+    next = degree * 5;
+    
+    for(pos = now; pos>=next; pos--){
+      myservo.write(pos);
+      delay(50);
+    }
+  }
+  
+  index = 0;
 }
