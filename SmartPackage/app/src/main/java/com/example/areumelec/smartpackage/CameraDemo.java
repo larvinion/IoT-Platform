1a69ad4d62d49fb55da68dba53d22c50ac021739
Larva <ssh9112@naver.com>
2016-11-28 21:15:24 +0900

    Add bluetooth module in CameraDemo.java

--- SmartPackage/app/src/main/java/com/example/areumelec/smartpackage/CameraDemo.java
+++ SmartPackage/app/src/main/java/com/example/areumelec/smartpackage/CameraDemo.java
@@ -1,7 +1,11 @@
 package com.example.areumelec.smartpackage;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.PixelFormat;
@@ -11,17 +15,27 @@ import android.os.Handler;
 import android.os.Message;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.example.areumelec.Bluetooth.BluetoothSerialClient;
+
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.LinkedList;
+import java.util.Set;
 
 import io.vov.vitamio.LibsChecker;
 import io.vov.vitamio.MediaPlayer;
@@ -72,6 +86,17 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
 
     private static final String TAG = "MediaPlayer";
 
+
+    private LinkedList<BluetoothDevice> mBluetoothDevices = new LinkedList<BluetoothDevice>();
+    private ArrayAdapter<String> mDeviceArrayAdapter;
+    private ProgressDialog mLoadingDialog;
+    private AlertDialog mDeviceListDialog;
+    private Menu mMenu;
+    private BluetoothSerialClient mClient;
+
+    private String data;
+    private byte[] buffer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,7 +119,6 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         captureBtn = (Button) findViewById(R.id.capture);
-        loadBtn = (Button) findViewById(R.id.load);
         leftLotateBtn = (ImageButton) findViewById(R.id.leftrotate);
         rightLotateBtn = (ImageButton) findViewById(R.id.rightrotate);
 
@@ -102,6 +126,17 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
         loadBtn.setOnClickListener(this);
         leftLotateBtn.setOnClickListener(this);
         rightLotateBtn.setOnClickListener(this);
+
+        mClient = BluetoothSerialClient.getInstance();
+
+        if(mClient == null) {
+            Toast.makeText(getApplicationContext(), "Cannot use the Bluetooth device.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        initProgressDialog();
+        initDeviceListDialog();
+
     }
 
     public void onClick(View v) {
@@ -118,15 +153,17 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
                     }
                 }, 2000);
                 break;
-            case R.id.load:
-                //SensorCheck activity로 가는 인텐트 생성
-//                Intent intent2 = new Intent(getApplicationContext(), SensorDemo.class);
-                //액티비티 시작!
-//                startActivity(intent2);
-                break;
             case R.id.leftrotate:
+                data +="l";
+                data += '\0';
+                buffer = data.getBytes();
+                mBTHandler.write(buffer);
                 break;
             case R.id.rightrotate:
+                data += "r";
+                data += '\0';
+                buffer = data.getBytes();
+                mBTHandler.write(buffer);
                 break;
         }
     }
@@ -219,6 +256,7 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
 
     protected void onPause() {
         super.onPause();
+        mClient.cancelScan(getApplicationContext());
         releaseMediaPlayer();
         doCleanUp();
     }
@@ -267,4 +305,187 @@ public class CameraDemo extends Activity implements OnBufferingUpdateListener, O
         mMediaPlayer.start();
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        enableBluetooth();
+    }
+    private void initProgressDialog() {
+        mLoadingDialog = new ProgressDialog(this);
+        mLoadingDialog.setCancelable(false);
+    }
+
+    private void initDeviceListDialog() {
+        mDeviceArrayAdapter = new ArrayAdapter<String>(getApplicationContext(), R.layout.item_device);
+        ListView listView = new ListView(getApplicationContext());
+        listView.setAdapter(mDeviceArrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item =  (String) parent.getItemAtPosition(position);
+                for(BluetoothDevice device : mBluetoothDevices) {
+                    if(item.contains(device.getAddress())) {
+                        connect(device);
+                        mDeviceListDialog.cancel();
+                    }
+                }
+            }
+        });
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select bluetooth device");
+        builder.setView(listView);
+        builder.setPositiveButton("Scan",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        scanDevices();
+                    }
+                });
+        mDeviceListDialog = builder.create();
+        mDeviceListDialog.setCanceledOnTouchOutside(false);
+    }
+
+    private void addDeviceToArrayAdapter(BluetoothDevice device) {
+        if(mBluetoothDevices.contains(device)) {
+            mBluetoothDevices.remove(device);
+            mDeviceArrayAdapter.remove(device.getName() + "\n" + device.getAddress());
+        }
+        mBluetoothDevices.add(device);
+        mDeviceArrayAdapter.add(device.getName() + "\n" + device.getAddress() );
+        mDeviceArrayAdapter.notifyDataSetChanged();
+    }
+
+    private void enableBluetooth() {
+        BluetoothSerialClient btSet =  mClient;
+        btSet.enableBluetooth(this, new BluetoothSerialClient.OnBluetoothEnabledListener() {
+            @Override
+            public void onBluetoothEnabled(boolean success) {
+                if(success) {
+                    getPairedDevices();
+                } else {
+                    finish();
+                }
+            }
+        });
+    }
+
+    private void getPairedDevices() {
+        Set<BluetoothDevice> devices =  mClient.getPairedDevices();
+        for(BluetoothDevice device: devices) {
+            addDeviceToArrayAdapter(device);
+        }
+    }
+
+    private void scanDevices() {
+        BluetoothSerialClient btSet = mClient;
+        btSet.scanDevices(getApplicationContext(), new BluetoothSerialClient.OnScanListener() {
+            String message ="";
+            @Override
+            public void onStart() {
+                Log.d("Test", "Scan Start.");
+                mLoadingDialog.show();
+                message = "Scanning....";
+                mLoadingDialog.setMessage("Scanning....");
+                mLoadingDialog.setCancelable(true);
+                mLoadingDialog.setCanceledOnTouchOutside(false);
+                mLoadingDialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        BluetoothSerialClient btSet = mClient;
+                        btSet.cancelScan(getApplicationContext());
+                    }
+                });
+            }
+
+            @Override
+            public void onFoundDevice(BluetoothDevice bluetoothDevice) {
+                addDeviceToArrayAdapter(bluetoothDevice);
+                message += "\n" + bluetoothDevice.getName() + "\n" + bluetoothDevice.getAddress();
+                mLoadingDialog.setMessage(message);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("Test", "Scan finish.");
+                message = "";
+                mLoadingDialog.cancel();
+                mLoadingDialog.setCancelable(false);
+                mLoadingDialog.setOnCancelListener(null);
+                mDeviceListDialog.show();
+            }
+        });
+    }
+
+    private void connect(BluetoothDevice device) {
+        mLoadingDialog.setMessage("Connecting....");
+        mLoadingDialog.setCancelable(false);
+        mLoadingDialog.show();
+        BluetoothSerialClient btSet =  mClient;
+        btSet.connect(getApplicationContext(), device, mBTHandler);
+    }
+    private BluetoothSerialClient.BluetoothStreamingHandler mBTHandler = new BluetoothSerialClient.BluetoothStreamingHandler() {
+        ByteBuffer mmByteBuffer = ByteBuffer.allocate(1024);
+
+        @Override
+        public void onError(Exception e) {
+            mLoadingDialog.cancel();
+            Toast.makeText(getApplicationContext(), "The the Bluetooth device Connection error - " +  e.toString(), Toast.LENGTH_SHORT).show();
+            mMenu.getItem(0).setTitle(R.string.action_connect);
+        }
+
+        @Override
+        public void onDisconnected() {
+            mMenu.getItem(0).setTitle(R.string.action_connect);
+            mLoadingDialog.cancel();
+            Toast.makeText(getApplicationContext(), "The the Bluetooth device Disconnected.", Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        public void onData(byte[] buffer, int length) {
+            if(length == 0) return;
+            if(mmByteBuffer.position() + length >= mmByteBuffer.capacity()) {
+                ByteBuffer newBuffer = ByteBuffer.allocate(mmByteBuffer.capacity() * 2);
+                newBuffer.put(mmByteBuffer.array(), 0,  mmByteBuffer.position());
+                mmByteBuffer = newBuffer;
+            }
+            mmByteBuffer.put(buffer, 0, length);
+            if(buffer[length - 1] == '\0') {
+                /* 데이터를 받아와서 집어넣는 부분 Buffer*/
+                //readSensorData(new String(mmByteBuffer.array(), 0, mmByteBuffer.position()));
+                mmByteBuffer.clear();
+            }
+        }
+
+        @Override
+        public void onConnected() {
+            Toast.makeText(getApplicationContext(), "Messgae : " + mClient.getConnectedDevice().getName() + "Connected. ", Toast.LENGTH_SHORT).show();
+            mLoadingDialog.cancel();
+            mMenu.getItem(0).setTitle(R.string.action_disconnect);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        mMenu = menu;
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean connect = mClient.isConnection();
+        if(item.getItemId() == R.id.action_connect) {
+            if (!connect) {
+                mDeviceListDialog.show();
+            } else {
+                mBTHandler.close();
+            }
+            return true;
+        }else {
+            return true;
+        }
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        mClient.claer();
+    };
 }
